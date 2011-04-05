4383f1d8cd88577253f05898a1ce98468ab4ac98
Colin DeCarlo <colin@thedecarlos.ca>
2011-04-05 07:02:40 -0400

    added the modified BSD license to the project

--- lib/YATA/Exception.php
+++ lib/YATA/Exception.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the YATA package.
+ *
+ * (c) Colin DeCarlo <colin@thedecarlos.ca>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace YATA;
 
 class Exception extends \Exception
